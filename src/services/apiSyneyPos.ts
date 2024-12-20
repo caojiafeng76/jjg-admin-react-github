ac32b893760afc69ec844d51a57bb03ff19b5b00
caojiafeng76 <bushishangdewugui@yahoo.com>
2024-12-20 16:19:27 +0800

    refactor(services): 重构 createPo 函数
    - 优化了订单创建的逻辑，使用 Promise.all 提高效率
    - 改进了错误处理，确保在任何步骤出错时都能正确抛出异常
    - 调整了代码结构，提高了可读性和可维护性

--- src/services/apiSyneyPos.ts
+++ src/services/apiSyneyPos.ts
@@ -54,6 +54,56 @@ export default async function getSyneyPo(id: string) {
   return data
 }
 
+// export async function createPo({
+//   po,
+//   map,
+// }: {
+//   po: ISyneyPo
+//   map: Map<string, ISyneyItem[]>
+// }) {
+//   map.forEach(async (items, key) => {
+//     const [SONo, SerialNo] = key.split('~')
+
+//     // console.log(po.SONo, po.SerialNo)
+//     const { data: poRepo, error: poError } = await supabase
+//       .from('syney-pos')
+//       .insert([{ ...po, SONo, SerialNo: +SerialNo }])
+//       .select()
+//       .single()
+
+//     if (poError) {
+//       console.error(poError)
+//       throw new Error('订单创建失败')
+//     }
+
+//     const poItems = items.map((item) => ({
+//       No: item.No,
+//       PartNo: item.PartNo,
+//       PartName: item.PartName,
+//       PartName2: item.PartName2,
+//       Spec: item.Spec,
+//       ParamSpec: item.ParamSpec,
+//       Unit: item.Unit,
+//       Qty: item.Qty,
+//       SONo: item.SONo,
+//       PartCode: item.PartCode,
+//       PartModel: item.PartModel,
+//       Remark: item.Remark,
+//       PoId: poRepo.id,
+//     }))
+
+//     const { error: itemsError } = await supabase
+//       .from('syney-po-items')
+//       .insert(poItems)
+
+//     if (itemsError) {
+//       await supabase.from('syney-pos').delete().eq('id', poRepo.id)
+//       console.error(itemsError)
+//       throw new Error('订单明细创建失败')
+//     }
+//   })
+// }
+
 export async function createPo({
   po,
   map,
@@ -61,47 +111,59 @@ export async function createPo({
   po: ISyneyPo
   map: Map<string, ISyneyItem[]>
 }) {
-  map.forEach(async (items, key) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const promises: any[] = []
+  // 遍历map
+  map.forEach((_items, key) => {
     const [SONo, SerialNo] = key.split('~')
-
-    // console.log(po.SONo, po.SerialNo)
-    const { data: poRepo, error: poError } = await supabase
+    const insertPoPromise = supabase
       .from('syney-pos')
       .insert([{ ...po, SONo, SerialNo: +SerialNo }])
       .select()
       .single()
-
-    if (poError) {
-      console.error(poError)
-      throw new Error('订单创建失败')
-    }
-
-    const poItems = items.map((item) => ({
-      No: item.No,
-      PartNo: item.PartNo,
-      PartName: item.PartName,
-      PartName2: item.PartName2,
-      Spec: item.Spec,
-      ParamSpec: item.ParamSpec,
-      Unit: item.Unit,
-      Qty: item.Qty,
-      SONo: item.SONo,
-      PartCode: item.PartCode,
-      PartModel: item.PartModel,
-      Remark: item.Remark,
-      PoId: poRepo.id,
-    }))
-
-    const { error: itemsError } = await supabase
-      .from('syney-po-items')
-      .insert(poItems)
-
-    if (itemsError) {
-      await supabase.from('syney-pos').delete().eq('id', poRepo.id)
-      console.error(itemsError)
-      throw new Error('订单明细创建失败')
-    }
+    promises.push(insertPoPromise)
   })
+
+  try {
+    const poRepoResults = await Promise.all(promises)
+    for (let i = 0; i < poRepoResults.length; i++) {
+      const { data: poRepo, error: poError } = poRepoResults[i]
+      if (poError) {
+        console.error(poError)
+        throw new Error('订单创建失败')
+      }
+      const items = Array.from(map.values())[i]
+      const poItems = items.map((item) => ({
+        No: item.No,
+        PartNo: item.PartNo,
+        PartName: item.PartName,
+        PartName2: item.PartName2,
+        Spec: item.Spec,
+        ParamSpec: item.ParamSpec,
+        Unit: item.Unit,
+        Qty: item.Qty,
+        SONo: item.SONo,
+        PartCode: item.PartCode,
+        PartModel: item.PartModel,
+        Remark: item.Remark,
+        PoId: poRepo.id,
+      }))
+
+      const { error: itemsError } = await supabase
+        .from('syney-po-items')
+        .insert(poItems)
+
+      if (itemsError) {
+        await supabase.from('syney-pos').delete().eq('id', poRepo.id)
+        console.error(itemsError)
+        throw new Error('订单明细创建失败')
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    // 这里可以根据具体需求进一步细化错误处理或者向外抛出更合适的错误提示等
+    throw error
+  }
 }
 
 export async function deletePo(ids: string[]) {
