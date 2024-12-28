b790c5d1a769e59b3f2559661cd894eb4b34c2d3
caojiafeng76 <bushishangdewugui@yahoo.com>
2024-12-28 14:23:04 +0800

    refactor(syney): 优化打印分解单按钮逻辑，清空选中的表格项

--- src/features/syney/PoList/PrintDecompositionButton.tsx
+++ src/features/syney/PoList/PrintDecompositionButton.tsx
@@ -1,15 +1,22 @@
 import { DocumentArrowDownIcon } from '@heroicons/react/16/solid'
 import { Button } from 'antd'
 import { usePrintDecomposition } from './usePrintDecomposition'
+import { useStore } from '@/store'
 
 export default function PrintDecompositionButton() {
   const { printDecomposition } = usePrintDecomposition()
+  const { setTableSelectedKeys } = useStore()
+
+  function onClick() {
+    printDecomposition()
+    setTableSelectedKeys([])
+  }
 
   return (
     <Button
       type="text"
       icon={<DocumentArrowDownIcon className="size-4 !text-red-500/80" />}
-      onClick={printDecomposition}
+      onClick={onClick}
     >
       打印分解单
     </Button>
