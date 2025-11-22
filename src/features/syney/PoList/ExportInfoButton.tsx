cac47cf3453c313af89d2dea6c81d2b65d891509
caojiafeng76 <bushishangdewugui@yahoo.com>
2025-11-22 15:34:43 +0800

    feat: Add buttons and hooks for exporting safe part info to Excel and printing decomposition.

--- src/features/syney/PoList/ExportInfoButton.tsx
+++ src/features/syney/PoList/ExportInfoButton.tsx
@@ -3,14 +3,18 @@ import { Button } from 'antd'
 import { useExportSafePartInfoAsExcel } from './useExportSafePartInfoAsExcel'
 
 export default function ExportInfoButton() {
-  const { exportSafePartInfoAsExcel } = useExportSafePartInfoAsExcel()
+  const { exportSafePartInfoAsExcel, contextHolder } =
+    useExportSafePartInfoAsExcel()
   return (
-    <Button
-      type="text"
-      icon={<DocumentArrowDownIcon className="size-4 !text-green-500/80" />}
-      onClick={exportSafePartInfoAsExcel}
-    >
-      导出安全部件信息
-    </Button>
+    <>
+      {contextHolder}
+      <Button
+        type="text"
+        icon={<DocumentArrowDownIcon className="size-4 !text-green-500/80" />}
+        onClick={exportSafePartInfoAsExcel}
+      >
+        导出安全部件信息
+      </Button>
+    </>
   )
 }
