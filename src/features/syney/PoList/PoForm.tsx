4481a4890c3170081701047e6fe3f71e31118c70
caojiafeng76 <bushishangdewugui@yahoo.com>
2025-11-25 14:42:50 +0800

    feat: Implement syney PO form and API service with initial mock data.

--- src/features/syney/PoList/PoForm.tsx
+++ src/features/syney/PoList/PoForm.tsx
@@ -1,5 +1,5 @@
 import { FC, Ref, memo } from 'react'
-import { DatePicker, Form, FormInstance, Input, Select } from 'antd'
+import { DatePicker, Form, FormInstance, Input } from 'antd'
 import { ISyneyPo } from '@/types'
 
 type PoFormProps = {
@@ -14,62 +14,6 @@ const layout = {
   labelCol: { span: 4 },
   wrapperCol: { span: 20 },
 }
-
-// 提取规格选项为常量,避免每次渲染重新创建
-const SPEC_OPTIONS = [
-  {
-    value: '1000型-室内-扶梯',
-    label: <span>1000型-室内-扶梯</span>,
-  },
-  {
-    value: '1000型-室外-扶梯',
-    label: <span>1000型-室外-扶梯</span>,
-  },
-  {
-    value: '1000型-室内-人行道',
-    label: <span>1000型-室内-人行道</span>,
-  },
-  {
-    value: '1000型-室外-人行道',
-    label: <span>1000型-室外-人行道</span>,
-  },
-  {
-    value: '800型-室内-扶梯',
-    label: <span>800型-室内-扶梯</span>,
-  },
-  {
-    value: '800型-室外-扶梯',
-    label: <span>800型-室外-扶梯</span>,
-  },
-  {
-    value: '800型-室内-人行道',
-    label: <span>800型-室内-人行道</span>,
-  },
-  {
-    value: '800型-室外-人行道',
-    label: <span>800型-室外-人行道</span>,
-  },
-  {
-    value: '600型-室内-扶梯',
-    label: <span>600型-室内-扶梯</span>,
-  },
-  {
-    value: '600型-室外-扶梯',
-    label: <span>600型-室外-扶梯</span>,
-  },
-  {
-    value: '1000型-室内-老围框',
-    label: <span>1000型-室内-老围框</span>,
-  },
-  {
-    value: '800型-室内-老围框',
-    label: <span>800型-室内-老围框</span>,
-  },
-  {
-    value: '600型-室内-老围框',
-    label: <span>600型-室内-老围框</span>,
-  },
-]
 
 const PoForm: FC<PoFormProps> = ({
   onFinish,
@@ -89,9 +33,6 @@ const PoForm: FC<PoFormProps> = ({
     >
       <Form.Item name="No" label="订单号" rules={[{ required: true }]}>
         <Input disabled={isCreating} />
-      </Form.Item>
-      <Form.Item name="Spec" label="规格" rules={[{ required: true }]}>
-        <Select disabled={isCreating} options={SPEC_OPTIONS} />
       </Form.Item>
       <Form.Item name="EndDate" label="交货日期" rules={[{ required: true }]}>
         <DatePicker disabled={isCreating} />
