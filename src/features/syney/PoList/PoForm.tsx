6b8ecce251f56f22f1d648a16fd21bb5076b7ac6
caojiafeng76 <bushishangdewugui@yahoo.com>
2025-11-25 14:52:11 +0800

    feat: add PoForm component for Purchase Order list management

--- src/features/syney/PoList/PoForm.tsx
+++ src/features/syney/PoList/PoForm.tsx
@@ -1,5 +1,5 @@
 import { FC, Ref, memo } from 'react'
-import { DatePicker, Form, FormInstance, Input } from 'antd'
+import { DatePicker, Form, FormInstance, Input, Select } from 'antd'
 import { ISyneyPo } from '@/types'
 
 type PoFormProps = {
@@ -14,6 +14,23 @@ const layout = {
   labelCol: { span: 4 },
   wrapperCol: { span: 20 },
 }
+
+// 规格选项
+const SPEC_OPTIONS = [
+  { value: '1000型-室内-扶梯', label: <span>1000型-室内-扶梯</span> },
+  { value: '1000型-室外-扶梯', label: <span>1000型-室外-扶梯</span> },
+  { value: '1000型-室内-人行道', label: <span>1000型-室内-人行道</span> },
+  { value: '1000型-室外-人行道', label: <span>1000型-室外-人行道</span> },
+  { value: '800型-室内-扶梯', label: <span>800型-室内-扶梯</span> },
+  { value: '800型-室外-扶梯', label: <span>800型-室外-扶梯</span> },
+  { value: '800型-室内-人行道', label: <span>800型-室内-人行道</span> },
+  { value: '800型-室外-人行道', label: <span>800型-室外-人行道</span> },
+  { value: '600型-室内-扶梯', label: <span>600型-室内-扶梯</span> },
+  { value: '600型-室外-扶梯', label: <span>600型-室外-扶梯</span> },
+  { value: '1000型-室内-老围框', label: <span>1000型-室内-老围框</span> },
+  { value: '800型-室内-老围框', label: <span>800型-室内-老围框</span> },
+  { value: '600型-室内-老围框', label: <span>600型-室内-老围框</span> },
+]
 
 const PoForm: FC<PoFormProps> = ({
   onFinish,
@@ -33,6 +50,14 @@ const PoForm: FC<PoFormProps> = ({
     >
       <Form.Item name="No" label="订单号" rules={[{ required: true }]}>
         <Input disabled={isCreating} />
+      </Form.Item>
+      <Form.Item name="Spec" label="规格">
+        <Select
+          disabled={isCreating}
+          options={SPEC_OPTIONS}
+          placeholder="留空则自动推断"
+          allowClear
+        />
       </Form.Item>
       <Form.Item name="EndDate" label="交货日期" rules={[{ required: true }]}>
         <DatePicker disabled={isCreating} />
